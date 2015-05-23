5fbd2ad3ea884fd50e984468b2d000e6f3ac3eaa
Travis Plunk <tplunk@microsoft.com>
2015-05-23 13:57:15 -0700

    Force Cim classes to register

--- Tests/MSFT_xWebBindingInformation.Tests.ps1
+++ Tests/MSFT_xWebBindingInformation.Tests.ps1
@@ -8,6 +8,10 @@ if($env:APPVEYOR_BUILD_VERSION)
 
 Import-Module (Join-Path $here -ChildPath "..\DSCResources\MSFT_xWebsite\MSFT_xWebsite.psm1")
 
+# Force Cim Classes to register
+$env:PSModulePath = "$env:PSModulePath;$here"
+Get-DscResource > $null
+
 Describe "MSFT_xWebBindingInformation" {
     $storeNames = (Get-CimClass -Namespace "root/microsoft/Windows/DesiredStateConfiguration" -ClassName "MSFT_xWebBindingInformation").CimClassProperties['CertificateStoreName'].Qualifiers['Values'].Value
     foreach ($storeName in $storeNames){
